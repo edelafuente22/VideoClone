1017b4db931690b1b52cdf50ae855cae5d0908c6
edelafuente22 <ofthefountain22@gmail.com>
2018-01-31 14:43:37 -0800

    minor changes; updated dependencies

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ class App extends Component {
             selectedVideo: null
         };
 
-        this.videoSearch('los angeles kings');
+        this.videoSearch('');
     }
 
     videoSearch(term) {
